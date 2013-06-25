cd6880cd153dd83b1c3c601d3f206991fae511cb
Yura Rodchyn <rodchyn@gmail.com>
2013-06-25 13:33:00 +0300

    Failed test

--- tests/RewriterTest.php
+++ tests/RewriterTest.php
@@ -7,6 +7,7 @@ class RewriterTest extends \PHPUnit_Framework_TestCase
     public function testSimpleAssign()
     {
         $rewriter = new Rewriter();
+//        var_dump(get_declared_classes());die;
         $this->assertEquals('$a = "b"', $rewriter->rewrite('a = "b"'));
     }
 }
